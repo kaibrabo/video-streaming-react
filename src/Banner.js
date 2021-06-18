618b613773048541dfc42bbd22e75c8b7509078d
Kainoa Brabo <kaibrabo@gmail.com>
2021-06-18 01:06:18 -0700

    fetch API from TMDB via Axios

--- src/Banner.js
+++ src/Banner.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import './Banner.css';
 
+import axios from './Axios';
+import requests from './Requests';
+
 function Banner() {
+
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+
+            setMovie(
+                request.data.results[
+                Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+
+            return request;
+        }
+
+        fetchData();
+    }, [])
+
+    console.log(movie);
 
     const truncate = (str, n) => {
         return str?.length > n ? str.substring(0, n - 1) + "..." : str;
@@ -13,12 +36,14 @@ function Banner() {
         <header className="banner"
             style={{
                 backgroundSize: 'cover',
-                backgroundImage: `url("https://upload.wikimedia.org/wikipedia/en/thumb/2/24/BlackMirrorTitleCard.jpg/250px-BlackMirrorTitleCard.jpg")`,
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
                 backgroundPosition: 'center center',
             }}>
 
             <div className="banner_contents">
-                <h1 className="banner_title">Movie Name</h1>
+                <h1 className="banner_title">{
+                movie?.title || movie?.name || movie?.original_name
+                }</h1>
                 <div className="banner_buttons">
                     <Button className="banner_button"
                         variant="contained"
@@ -31,7 +56,7 @@ function Banner() {
                 </div>
                 <h1 className="banner_description">
                     {truncate(
-                        `This is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test description`, 150
+                        movie?.overview, 150
                     )}</h1>
             </div>
 
