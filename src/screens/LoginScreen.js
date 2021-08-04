c79bba835b73e03d1b7ab503d06a51312254df7e
Kainoa Brabo <kaibrabo@gmail.com>
2021-08-03 22:43:50 -0700

    add landing title

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,13 +1,32 @@
 import React from "react";
-import './LoginScreen.css'
+import "./LoginScreen.css";
 
 function LoginScreen() {
 	return (
 		<div className="login_screen">
 			<div className="login_screen_background">
-                <img className="login_screen_logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
-                <button className="login_screen_button">Sign In</button>
-            </div>
+				<img
+					className="login_screen_logo"
+					src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+					alt=""
+				/>
+				<button className="login_screen_button">Sign In</button>
+				<div className="login_screen_gradient" />
+				<div className="login_screen_body">
+					<>
+						<h1>Unlimited films , TV programs and more.</h1>
+                        <h2>Watch anywhere. Cancel at anytime.</h2>
+                        <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+                        <div className="login_screen_input">
+                            <form action="">
+                                <input type="email"
+                                placeholder="Email Address" />
+                                <button className="login_screen_get_started">GET STARTED</button>
+                            </form>
+                        </div>
+					</>
+				</div>
+			</div>
 		</div>
 	);
 }
