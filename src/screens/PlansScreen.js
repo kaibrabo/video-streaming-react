ead158ddb7d8e6e60c55cdb34d692cf037ca054b
Kainoa Brabo <kaibrabo@gmail.com>
2021-11-23 21:33:57 -0800

    plans css

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -1,35 +1,61 @@
 import { useState, useEffect } from "react";
-import {db} from "../firebase";
+import { db } from "../firebase";
 import "./PlansScreen.css";
 
 function PlansScreen() {
-    const [products, setProducts] = useState([]);
+	const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        db.collection("products")
-            .where("active", "==", true)
-            .get()
-            .then(qs => {
-                const _products = {};
+	useEffect(() => {
+		db.collection("products")
+			.where("active", "==", true)
+			.get()
+			.then((qs) => {
+				const _products = {};
 
-                qs.forEach(async productDoc => {
-                    _products[productDoc.id] = productDoc.data()
-                    const priceSnap = await productDoc.ref.collection('prices').get();
-                    priceSnap.docs.forEach(price => {
-                        _products[productDoc.id].prices = {
-                            priceId: price.id,
-                            priceData: price.data()
-                        }
-                    })
-                });
+				qs.forEach(async (productDoc) => {
+					_products[productDoc.id] = productDoc.data();
+					const priceSnap = await productDoc.ref
+						.collection("prices")
+						.get();
+					priceSnap.docs.forEach((price) => {
+						_products[productDoc.id].prices = {
+							priceId: price.id,
+							priceData: price.data(),
+						};
+					});
+				});
 
-                setProducts(_products)
-            });
-    }, []);
-    
-    console.log(products);
+				setProducts(_products);
+			});
+	}, []);
 
-    return <div className="plans_screen"></div>;
+	console.log(products);
+	const loadCheckout = async (priceId) => {};
+
+	return (
+		<div className="plans_screen">
+			{Object.entries(products)
+				.reverse()
+				.map(([productId, productData]) => {
+					// Logic to check if users subscription is active
+					return (
+						<div className="plans_screen_plans" key={productId}>
+							<div className="plans_screen_plans_info">
+								<h5>{productData.name}</h5>
+								<h6>{productData.description}</h6>
+							</div>
+							<button
+								onClick={() =>
+									loadCheckout(productData?.prices?.priceId)
+								}
+							>
+								choose
+							</button>
+						</div>
+					);
+				})}
+		</div>
+	);
 }
 
 export default PlansScreen;
